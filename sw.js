0f637b06d2bb43383b1aca56bb47ab7f5a3721f0
bsg-univers <trubitsyna.anastasiia@gmail.com>
2025-12-29 12:32:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
 
-// Cette partie permet de garder le script "vivant" pendant les requêtes API
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes('script.google.com')) {
-    event.respondWith(
-      fetch(event.request).catch(err => {
-        return new Response(JSON.stringify({error: "Offline"}), {
-          headers: { 'Content-Type': 'application/json' }
-        });
-      })
-    );
-  } else {
-    event.respondWith(fetch(event.request));
-  }
-});

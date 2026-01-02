0d4c7b4e799f159756254dbcaedca6a051371381
bsg-univers <trubitsyna.anastasiia@gmail.com>
2026-01-02 20:27:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,14 @@
+const CACHE_NAME = 'bsg-player-v1';
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+// Permet de garder le SW éveillé pendant la récupération des flux audio
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request));
 });
